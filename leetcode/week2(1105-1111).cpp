a193567d13b0221086d7876d58c2f0a17d2efd4a
Wakeen <zhouweikang01@baidu.com>
2018-11-05 19:33:01 +0800

    [2018-11-05][easy-350]

--- leetcode/week2(1105-1111).cpp
+++ leetcode/week2(1105-1111).cpp
@@ -1,21 +1,20 @@
 //
-//  main.cpp
+//  week2(1105-1111).cpp
 //  leetcode
 //
-//  Created by Zhou,Weikang on 2018/10/31.
-//  Copyright © 2018年 Zhou,Weikang. All rights reserved.
+//  Created by Zhou,Weikang on 2018/11/5.
+//  Copyright © 2018 Zhou,Weikang. All rights reserved.
 //
 
-#include <iostream>
-#include "week1(1031-1104).hpp"
+#include "week2(1105-1111).hpp"
 #include <vector>
 #include <string>
 
 using namespace std;
 
-
 class Solution {
 public:
+//https://leetcode.com/problems/intersection-of-two-arrays-ii/description/
     vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
         if (nums1.empty() || nums1.empty())
         {
@@ -37,17 +36,3 @@ public:
         return result;
     }
 };
-
-
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    Solution *solution = new Solution();
-    vector<int>nums2 = {2, 2};
-    vector<int>nums1 = {1, 2, 2, 1};
-    vector<int>result = solution->intersect(nums1, nums2);
-    for (int i=0; i<result.size(); i++) {
-        cout<<result[i]<<" ";
-    }
-    cout<<endl;
-    return 0;
-}
---
+++ leetcode/week2(1105-1111).hpp
@@ -0,0 +1,14 @@
+//
+//  week2(1105-1111).hpp
+//  leetcode
+//
+//  Created by Zhou,Weikang on 2018/11/5.
+//  Copyright © 2018 Zhou,Weikang. All rights reserved.
+//
+
+#ifndef week2_1105_1111__hpp
+#define week2_1105_1111__hpp
+
+#include <stdio.h>
+
+#endif /* week2_1105_1111__hpp */
